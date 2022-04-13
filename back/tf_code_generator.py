8eeb8c4991f5ffca7ca7ec76e2d8784c874228f3
Anish <anishpahilajani@eduroam-169-233-253-223.ucsc.edu>
2022-04-13 09:32:31 -0700

    cnn code added

--- back/tf_code_generator.py
+++ back/tf_code_generator.py
@@ -11,17 +11,34 @@ def tf_Code_generator(l):
     f = open("tf.py", "a")
     f.write("model = tf.keras.Sequential([\n")
     final_String += "model = tf.keras.Sequential([\n"
+    FlattenFlag = 0
     for i in range(len(l)):
         if (l[i][0] == 0): # if Dense
+            if (FlattenFlag == 1):
+                FlattenFlag = 0
+                f.write("   tf.keras.layers.Flatten(),\n")
+                final_String+= "   tf.keras.layers.Flatten(),\n"
+                
+                
             if (i == 0): # for first hidden layer
                 f.write("   tf.keras.layers.Flatten(),\n")
                 final_String += "   tf.keras.layers.Flatten(),\n"
                 f.write("   tf.keras.layers.Dense("+str(l[i][2])+", input_shape=("+str(l[i][1])+",), activation='"+str(operate_type[l[i][3]])+"'),\n")
                 final_String += "   tf.keras.layers.Dense("+str(l[i][2])+", input_shape=("+str(l[i][1])+",), activation='"+str(operate_type[l[i][3]])+"'),\n"
+                FlattenFlag = 0
             else:
                 f.write("   tf.keras.layers.Dense("+str(l[i][2])+", activation='"+str(operate_type[l[i][3]])+"'),\n")
                 final_String += "   tf.keras.layers.Dense("+str(l[i][2])+", activation='"+str(operate_type[l[i][3]])+"'),\n"
         
+        if(l[i][0] == 1): # if Conv2D
+            convString = "   tf.keras.layers.Conv2D("+str(l[i][1])+ ", kernel_size="+str(l[i][-1])+", activation='"+str(operate_type[l[i][3]])+"'),\n"
+            f.write(convString)
+            final_String+=convString
+            FlattenFlag = 1
+        if (l[i][0]==4): # if MaxPool
+            maxPoolString = "   tf.keras.layers.MaxPool2D(pool_size="+l[i][-1]+"),\n"
+            f.write(maxPoolString)
+            final_String+=maxPoolString
                 
     f.write("])")
     final_String += "])"
@@ -32,16 +49,15 @@ def tf_Code_generator(l):
     
     
 if __name__ == "__main__":
-    operate_type = {0:'Dense', 1:'Conv2D', 2:'relu', 3:'softmax'}
+    operate_type = {0:'Dense', 1:'Conv2D', 2:'relu', 3:'softmax', 4:'MaxPool'}
     layersDense = [[0,784,50,2], [0,50,30,2], [0,30,10,3]]
     #layersDense[0] = [operator_type, Input shape(prev layer input) ,Tensorshape(no.of neurons in layer), operation_type(activation)]
 
 
-    layersConv = [[1,24,2, 3, "kernel size"], [1,50,30,2, ""], [0,30,10,3]]
-    #print(layersConv[0])
-    #layersDense[0] = [operator_type, Input shape(prev layer input) ,Tensorshape(number of filters), operation_type(activation), string(kernel size)]
+    layersConv = [[1, 24,00, 2, "(3,3)"], [4,00,00,00, "(2,2)"], [1, 36,00, 2, "(3,3)"], [4,00,00,00, "(2,2)"], [0,784,128,2], [0,128,10,2]]
+    #layersDense[0] = [operator_type, num_filters, shape(prev layer input DC) , operation_type(activation), string(kernel size)/(pool_size)]
 
-    a = tf_Code_generator(layersDense)
+    a = tf_Code_generator(layersConv)
     print(a)
             
         
