a2d9c36bd0ebc52434950132a323c0d48724bc00
Anish <anishpahilajani@AnishsMacBookPro.local>
2022-05-07 14:40:23 -0700

    Added some more layers, more activation functions, and more loss functions

--- front/public/tf_code_generator.py
+++ front/public/tf_code_generator.py
@@ -1,67 +1,116 @@
-operate_type = {0:'Dense', 1:'Conv2D', 2:'relu', 3:'softmax', 4:'MaxPool'}
-
-def tf_code_generator(l):
+def tf_Code_generator(l):
+    operate_type = {0:'Dense', 1:'Conv2D', 2:'relu', 3:'softmax', 4:'MaxPool', 5:'sigmoid', 6:'softplus', 7:'swish', 8:'softsign', 9:'tanh', 10:'AveragePooling2D', 11:'GlobalAveragePooling2D'}
     final_String = ""
     headerString = "import tensorflow as tf \nfrom tensorflow import keras\n\n"
     final_String += headerString
 
-    
+    # f = open("tf.py", "w")
+    # f.write(headerString)
+    # f.close()
 
+
+    # f = open("tf.py", "a")
+    # f.write("model = tf.keras.Sequential([\n")
     final_String += "model = tf.keras.Sequential([\n"
     FlattenFlag = 0
     for i in range(len(l)):
         if (l[i][0] == 0): # if Dense
             if (FlattenFlag == 1):
                 FlattenFlag = 0
+                # f.write("   tf.keras.layers.Flatten(),\n")
                 final_String+= "   tf.keras.layers.Flatten(),\n"
                 
                 
             if (i == 0): # for first hidden layer
+                # f.write("   tf.keras.layers.Flatten(),\n")
                 final_String += "   tf.keras.layers.Flatten(),\n"
+                # f.write("   tf.keras.layers.Dense("+str(l[i][2])+", input_shape=("+str(l[i][1])+",), activation='"+str(operate_type[l[i][3]])+"'),\n")
                 final_String += "   tf.keras.layers.Dense("+str(l[i][2])+", input_shape=("+str(l[i][1])+",), activation='"+str(operate_type[l[i][3]])+"'),\n"
                 FlattenFlag = 0
             else:
+                # f.write("   tf.keras.layers.Dense("+str(l[i][2])+", activation='"+str(operate_type[l[i][3]])+"'),\n")
                 final_String += "   tf.keras.layers.Dense("+str(l[i][2])+", activation='"+str(operate_type[l[i][3]])+"'),\n"
         
         if(l[i][0] == 1): # if Conv2D
             convString = "   tf.keras.layers.Conv2D("+str(l[i][1])+ ", kernel_size="+str(l[i][-1])+", activation='"+str(operate_type[l[i][3]])+"'),\n"
-            
+            # f.write(convString)
             final_String+=convString
             FlattenFlag = 1
         if (l[i][0]==4): # if MaxPool
             maxPoolString = "   tf.keras.layers.MaxPool2D(pool_size="+l[i][-1]+"),\n"
-            
+            # f.write(maxPoolString)
+            final_String+=maxPoolString
+        if (l[i][0]==10): # if AveragePooling2D
+            maxPoolString = "   tf.keras.layers.AveragePooling2D(pool_size="+l[i][-1]+"),\n"
+            # f.write(maxPoolString)
+            final_String+=maxPoolString
+        if (l[i][0]==11): # if GlobalAveragePooling2D
+            maxPoolString = "   tf.keras.layers.GlobalAveragePooling2D(pool_size="+l[i][-1]+"),\n"
+            # f.write(maxPoolString)
             final_String+=maxPoolString
                 
-    
+    # f.write("])")
     final_String += "])"
-    
+    # f.close()
     return final_String
 
-def tf_train_model(optimizer, loss):
-    
+def train_model(optimizer, loss):
+    #f = open("tf.py", "a") #
     final_string = ""
     a = "\n\nmodel.compile(optimizer="
     final_string += a
-    
-    if (optimizer == 6): #SGD
+    #f.write(a) #
+    if (optimizer == "SGD"):
         final_string += "tf.keras.optimizers.SGD(learning_rate=1e-1),\n"
-        
-    if (optimizer == 0): #Adam 
+        # f.write("tf.keras.optimizers.SGD(learning_rate=1e-1),\n")
+    if (optimizer == "Adam"):
         final_string += "tf.keras.optimizers.Adam(learning_rate=1e-3),\n"
-        
-    if (loss == 0): #Cross categorical crossentropy
-        a = "               loss='sparse_categorical_crossentropy',\n"
+        # f.write("tf.keras.optimizers.Adam(learning_rate=1e-3),\n")
+    if (optimizer == "Adadelta"):
+        final_string += "tf.keras.optimizers.Adadelta(learning_rate=1e-3),\n"
+        #f.write("tf.keras.optimizers.Adadelta(learning_rate=1e-3),\n") #
+    if (optimizer == "Adagrad"):
+        final_string += "tf.keras.optimizers.Adagrad(learning_rate=1e-3),\n"
+        #f.write("tf.keras.optimizers.Adagrad(learning_rate=1e-3),\n") #
+    if (optimizer == "Adamax"):
+        final_string += "tf.keras.optimizers.Adamax(learning_rate=1e-3),\n"
+        #f.write("tf.keras.optimizers.Adamax(learning_rate=1e-3),\n") #
+    if (optimizer == "RMSprop"):
+        final_string += "tf.keras.optimizers.RMSprop(learning_rate=1e-3),\n"
+        #f.write("tf.keras.optimizers.RMSprop(learning_rate=1e-3),\n") #
+    if (optimizer == "Nadam"):
+        final_string += "tf.keras.optimizers.Nadam(learning_rate=1e-3),\n"
+        #f.write("tf.keras.optimizers.Nadam(learning_rate=1e-3),\n") #
+    if (loss == "sparse_categorical_crossentropy"):
+        a = "               loss='"+loss+"',\n"
         final_string += a
-        
-    
+        # f.write(a) #
+    if (loss == "CategoricalCrossentropy"):
+        a = "               loss='"+loss+"',\n"
+        final_string += a
+        #f.write(a) #
+    if (loss == "MeanAbsoluteError"):
+        a = "               loss='"+loss+"',\n"
+        final_string += a
+        #f.write(a) #
+    if (loss == "Hinge"):
+        a = "               loss='"+loss+"',\n"
+        final_string += a
+        #f.write(a) #
+    if (loss == "huber"):
+        a = "               loss='"+loss+"',\n"
+        final_string += a
+        #f.write(a) #
+    if (loss == "MeanSquaredError"):
+        a = "               loss='"+loss+"',\n"
+        final_string += a
+        #f.write(a) #
+    #f.write("               metrics=['accuracy'])") #
     final_string+= "metrics=['accuracy'])"
     return final_string
 
     
-
-
-
+    
         
         
     
@@ -70,20 +119,19 @@ def tf_train_model(optimizer, loss):
     
     
 if __name__ == "__main__":
-    
+    operate_type = {0:'Dense', 1:'Conv2D', 2:'relu', 3:'softmax', 4:'MaxPool', 5:'sigmoid', 6:'softplus', 7:'swish', 8:'softsign', 9:'tanh', 10:'AveragePooling2D', 11:'GlobalAveragePooling2D'}
     layersDense = [[0,784,50,2], [0,50,30,2], [0,30,10,3]]
     #layersDense[0] = [operator_type, Input shape(prev layer input) ,Tensorshape(no.of neurons in layer), operation_type(activation)]
 
 
-    layersConv = [[1, 24,00, 2, "(3,3)"], [4,00,00,00, "(2,2)"], [1, 36,00, 2, "(3,3)"], [4,00,00,00, "(2,2)"], [0,784,128,2], [0,128,10,2]]
+    layersConv = [[1, 24,00, 2, "(3,3)"], [4,00,00,00, "(2,2)"], [1, 36,00, 2, "(3,3)"], [11,00,00,00, "(2,2)"], [0,784,128,2], [0,128,10,2]]
     #layersDense[0] = [operator_type, num_filters, shape(prev layer input DC) , operation_type(activation), string(kernel size)/(pool_size)]
 
-    a = tf_code_generator(layersConv)
+    a = tf_Code_generator(layersConv)
     print(a)
-    
-    optimizer = {0:'SGD', 1:'Adam'}
-    loss = {0: 'sparse_categorical_crossentropy'}
-    a = train_model(optimizer[1], loss[0])
+    optimizer = {0:'SGD', 1:'Adam', 3:"Adadelta", 4:"Adagrad", 5:"Adamax", 6:"RMSprop", 7:"Nadam"}
+    loss = {0: 'sparse_categorical_crossentropy', 1:"CategoricalCrossentropy", 2:"MeanAbsoluteError", 3:"Hinge", 4:"huber", 5:"MeanSquaredError"}
+    a = train_model(optimizer[7], loss[5])
     print(a)
     
             
