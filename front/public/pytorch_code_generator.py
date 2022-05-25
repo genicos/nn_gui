d2cca39c59b81238a535bde30458df92773d6d97
Kat1650 <katn1650@gmail.com>
2022-05-24 20:49:07 -0700

    Added the last few layers, up to date to match tf code generator

--- front/public/pytorch_code_generator.py
+++ front/public/pytorch_code_generator.py
@@ -1,5 +1,5 @@
 def pytorch_code_generator(l):
-    operate_type = {1:'identity', 2:'Dense', 3:'Conv2D', 4:'relu', 5:'softmax', 6:'MaxPool', 12:'sigmoid', 13:'softplus', 14:'swish', 15:'softsign', 16:'tanh', 9:'AveragePooling2D', 10:'GlobalAveragePooling2D'}
+    operate_type = {1:'identity', 2:'Dense', 3:'Conv2D', 4:'relu', 5:'softmax', 6:'MaxPool', 7:'ZeroPaddingLayer', 8:'BatchNormalization' ,12:'sigmoid', 13:'softplus', 14:'swish', 15:'softsign', 16:'tanh', 9:'AveragePooling2D', 10:'GlobalAveragePooling2D'}
     # Imports
     final_String = ""
     headerString = "import torch\nimport torchvision\nimport torch.nn as nn\nimport torch.optim as optim\n\n" 
@@ -66,17 +66,17 @@ def pytorch_code_generator(l):
             kernel = int(kernel / 2)
 
         if (l[i][0]==11): # if GlobalAveragePooling2D
-            
-            # NO GlobalAveragePooling2D in pytorch!!!!
-            # https://www.kaggle.com/general/222335
-            # possible way of doing it but idk
-            
-            #kernel = int(kernel / 2)
-            final_String # += "        self.pool" + str(i) + " = nn.AvgPool2d("+str(K) + ")\n"
+            kernel = int(kernel / 2)
+            final_String  += "        self.pool" + str(i) + " = nn.AdaptiveAvgPool2d("+ str(kernel) + ")\n"
         
         if (l[i][0]==1): # identity
             final_String += "        self.id" + str(i) + " = nn.Identity(" +l[i][5]+"),\n"
-            
+
+        if (l[i][0]==7): # ZeroPadding2D
+            final_String += "        self.pad" + str(i) +  " = nn.ZeroPad2d(" +l[i][5].split(":")[0] +")\n"
+        
+        if (l[i][0]==8): # BatchNormalization
+            final_String += "        self.batch" + str(i) +  " = nn.BatchNorm2d(" + str(prev_out)+")\n"
 
     # Activation of layers and other stuff (pytorch requires these to be set in forward, hence why its formatted like this)
     final_String += "\n    def forward(self, x):\n"
@@ -103,8 +103,17 @@ def pytorch_code_generator(l):
         if (l[i][0]==10): # if AveragePooling2D
             final_String += "        x = self.pool" + str(i) + "(x)\n"
 
-        if (l[i][0]==1): # if MaxPool
+        if (l[i][0]==11): # if GlobalAveragePooling2D
+            final_String += "        x = self.pool" + str(i) + "(x)\n"
+            
+        if (l[i][0]==1): # if identity
             final_String += "        x = self.id" + str(i) + "(x)\n"
+
+        if (l[i][0]==7): # ZeroPadding2D
+            final_String += "        x = self.pad" + str(i) + "(x)\n"
+        
+        if (l[i][0]==8): # BatchNormalization
+            final_String += "        x = self.batch" + str(i) + "(x)\n"
 
     # Saving this all to "net" as our model
     final_String += "\nnet = Net()\n"
@@ -114,14 +123,14 @@ def pytorch_code_generator(l):
 
 
 def pytorch_train_model(optimizer, loss):
-    # No real equivalent to model.compile in pytorch :/
-    final_string = ""
-
+    
     optimizer_selection = {6:'SGD', 0:'Adam', 2:"Adadelta", 3:"Adagrad", 4:"Adamax", 5:"RMSprop", 1:"Nadam"}
     loss_selection = {0:"CategoricalCrossentropy", 1:"MeanAbsoluteError", 2:"Hinge", 3:"huber", 4:"MeanSquaredError"}
-
+    
     optimizer = optimizer_selection[optimizer]
     loss = loss_selection[loss]
+
+    final_string = ""
     
     # Optimizers
     a = "\noptimizer = "
@@ -151,8 +160,8 @@ def pytorch_train_model(optimizer, loss):
     # Loss 
     final_string += "criterion = "
     
-    # if (loss == "sparse_categorical_crossentropy"):
-    #     final_string += ""
+    if (loss == "sparse_categorical_crossentropy"):
+        final_string += "nn.CrossEntropyLoss()\n"
 
     if (loss == "CategoricalCrossentropy"):
         final_string += "nn.CrossEntropyLoss()\n"
@@ -167,7 +176,7 @@ def pytorch_train_model(optimizer, loss):
         final_string += "nn.HuberLoss()\n"
     
     if (loss == "MeanSquaredError"):
-        final_string += "nn.MSELoss()\m"
+        final_string += "nn.MSELoss()\n"
 
     # Determine metrics (look if there is an equivlanet in Pytorch)
     # Didn't find anything equivalent for Pytorch :/
@@ -177,7 +186,6 @@ def pytorch_train_model(optimizer, loss):
 
 
 if __name__ == "__main__":
-    # NEW
     operate_type = {0:'Dense', 1:'Conv2D', 2:'relu', 3:'softmax', 4:'MaxPool', 5:'sigmoid', 6:'softplus', 7:'swish', 8:'softsign', 9:'tanh', 10:'AveragePooling2D', 11:'GlobalAveragePooling2D'}
     #layersDense = [[0,784,50,2], [0,50,30,2], [0,30,10,3]]# layersDense = [[0,784,50,0,0,2]
     #layersDense[0] = [operator_type, Input shape(prev layer input) ,Tensorshape(no.of neurons in layer), operation_type(activation)]
@@ -185,7 +193,7 @@ if __name__ == "__main__":
     
     # [1, 0,0,0, MISC(24 (3,3))]
     layersConv = [[1, 24,00, 2, "(3,3)"], [4,00,00,00, "(2,2)"], [1, 36,00, 2, "(3,3)"], [11,00,00,00, "(2,2)"], [0,784,128,2], [0,128,10,2]]
-    layersConv = [[3,[28,28,1],784,[26,26,32],1875,"(3,3):24",4], [6,0,0,0,0,"(2,2):None"], [10,0,0,0,0,"None:False"], [2,[28,28],784,[128],128,"Misc",4]]
+    layersConv = [[3,[28,28,1],784,[26,26,32],1875,"(3,3):24",4], [6,0,0,0,0,"(2,2):None"], [10,0,0,0,0,"None:False"], [2,[28,28],784,[128],128,"Misc",4], [7,[28,28],784,[128],128,"(1,1):",4], [8, "No one cares"]] 
     #None represents the strides for MaxPool2D
     #layersDense[0] = [operator_type, num_filters, shape(prev layer input DC) , operation_type(activation), string(kernel size)/(pool_size)]
 
@@ -193,7 +201,6 @@ if __name__ == "__main__":
     print(a)
     optimizer = {6:'SGD', 0:'Adam', 2:"Adadelta", 3:"Adagrad", 4:"Adamax", 5:"RMSprop", 1:"Nadam"}
     loss = {0:"CategoricalCrossentropy", 1:"MeanAbsoluteError", 2:"Hinge", 3:"huber", 4:"MeanSquaredError"}
-
-    # nico changed pytorch_train_model so that it just needs integers
+    
     a = pytorch_train_model(6, 4)
     print(a)
